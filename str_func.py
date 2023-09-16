5e0475d813734710136f68cf96595c33b78c1cdb
NickKrlv <Nick.krlv@yandex.ru>
2023-09-16 08:55:45 +0300

    fix func

--- str_func.py
+++ str_func.py
@@ -1,8 +1,6 @@
-def title_func(some_text):
-    """Докстриг, который конфликтовал, но теперь все нормально"""
-    return some_text.title()
-
-
 def upper_func(some_text):
     """Принимает строку и возвращает ее в верхнем регистре"""
     return some_text.upper()
+def title_func(some_text):
+    """Принимает строку и возвращает с заглавными буквами"""
+    return some_text.title()
